cef8dfe8a0e4f134947988afda99492348473a66
kwekre <165562887+kwekre@users.noreply.github.com>
2026-09-01 16:33:08 +0800

    update probekit/detectors/__init__.py

--- probekit/detectors/__init__.py
+++ probekit/detectors/__init__.py
@@ -3,8 +3,17 @@ from .sqli import SQLiDetector
 from .xss import XSSDetector
 from .ssrf import SSRFDetector
 from .openredirect import OpenRedirectDetector
+from .command_injection import CommandInjectionDetector
+from .path_traversal import PathTraversalDetector
+from .jwt import JwtDetector
 
-ALL_DETECTORS = [SQLiDetector, XSSDetector, SSRFDetector, OpenRedirectDetector]
+ALL_DETECTORS = [
+    SQLiDetector, XSSDetector, SSRFDetector, OpenRedirectDetector,
+    CommandInjectionDetector, PathTraversalDetector, JwtDetector,
+]
 
-__all__ = ["Detector", "SQLiDetector", "XSSDetector", "SSRFDetector",
-           "OpenRedirectDetector", "ALL_DETECTORS"]
+__all__ = [
+    "Detector", "SQLiDetector", "XSSDetector", "SSRFDetector",
+    "OpenRedirectDetector", "CommandInjectionDetector",
+    "PathTraversalDetector", "JwtDetector", "ALL_DETECTORS",
+]
